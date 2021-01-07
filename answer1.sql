bd9fcc970bdc16791bf35a1d3a57c24e699ae193
icebox827 <icebox827@gmail.com>
2021-01-07 14:09:08 -0500

    finish select quiz

--- answer1.sql
+++ answer1.sql
@@ -10,3 +10,25 @@ CASE WHEN
 subject IN ('Physics','Chemistry')
 THEN 1 ELSE 0 END,
 subject, winner;
+
+/* Select Quiz */
+
+SELECT winner FROM nobel
+ WHERE winner LIKE 'C%' AND winner LIKE '%n'
+
+SELECT COUNT(subject) FROM nobel
+ WHERE subject = 'Chemistry'
+   AND yr BETWEEN 1950 and 1960
+
+SELECT DISTINCT yr
+  FROM nobel
+ WHERE subject='Medicine' 
+   AND yr NOT IN(SELECT yr FROM nobel 
+                  WHERE subject='Literature')
+   AND yr NOT IN (SELECT yr FROM nobel
+                   WHERE subject='Peace')
+
+SELECT yr FROM nobel
+ WHERE yr NOT IN(SELECT yr 
+                   FROM nobel
+                 WHERE subject IN ('Chemistry','Physics'))
