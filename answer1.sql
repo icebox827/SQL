122a04ea452c5532f76eddf8c3fece1b6a854a6e
icebox827 <icebox827@gmail.com>
2021-01-07 15:41:39 -0500

    done with nested select quiz

--- answer1.sql
+++ answer1.sql
@@ -121,3 +121,19 @@ FROM world y
 WHERE y.continent = x.continent
     AND y.population <> x.population
     AND population>0);
+
+/* Select in select quiz */
+
+SELECT name FROM bbc
+ WHERE population < (SELECT population FROM bbc WHERE name='Russia')
+   AND population > (SELECT population FROM bbc WHERE name='Denmark')
+
+SELECT name FROM bbc
+ WHERE gdp > (SELECT MAX(gdp) FROM bbc WHERE region = 'Africa')
+
+SELECT name, region FROM bbc x
+ WHERE population < ALL (SELECT population/3 FROM bbc y WHERE y.region = x.region AND y.name != x.name)
+
+ SELECT name,region,population FROM bbc x WHERE 50000 < ALL (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0)
+
+ SELECT region, name, population FROM bbc x WHERE population <= ALL (SELECT population FROM bbc y WHERE y.region=x.region AND population>0)
