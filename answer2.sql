709ecf58de4e24127157e09a71f8081dac9763ea
icebox827 <icebox827@gmail.com>
2021-01-07 17:55:54 -0500

    add first 6 answers

--- answer2.sql
+++ answer2.sql
@@ -9,3 +9,21 @@ SELECT name, population/area AS density FROM bbc WHERE population = (SELECT MAX(
 SELECT AVG(population) FROM bbc WHERE name IN ('Poland', 'Germany', 'Denmark')
 
 SELECT COUNT(name) FROM bbc WHERE population < 150000
+
+-- JOIN answers 
+
+SELECT matchid, player FROM goal 
+  WHERE teamid = 'GER'
+
+SELECT id,stadium,team1,team2
+  FROM game
+    WHERE id = 1012
+
+SELECT player, teamid, stadium, mdate
+  FROM game JOIN goal ON (id=matchid)
+    WHERE teamid = 'GER'
+
+SELECT team1, team2, player 
+  FROM game JOIN goal ON (id=matchid)
+    WHERE player like 'Mario%'
+
