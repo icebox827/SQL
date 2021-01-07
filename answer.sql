306ffc97758cdf53acf78a6a81a1d34f4a289cc5
icebox827 <icebox827@gmail.com>
2021-01-07 12:37:47 -0500

    add nwe answewr

--- answer.sql
+++ answer.sql
@@ -28,3 +28,64 @@ SELECT name FROM world
 SELECT name, population
   FROM world
  WHERE population BETWEEN 1000000 AND 1250000
+
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+SELECT name FROM world
+  WHERE name LIKE '%y'
+
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+SELECT name FROM world
+ WHERE name IN ('Cuba', 'Togo')
+
+SELECT name, population
+  FROM world
+ WHERE continent IN ('Europe', 'Asia')
+
+SELECT population
+  FROM world
+ WHERE name = 'United Kingdom'
+
+SELECT name
+  FROM world
+ WHERE name LIKE 'U%'
+
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+SELECT yr, subject
+  FROM nobel
+WHERE winner = 'Albert Einstein'
+
+SELECT winner
+  FROM nobel
+WHERE subject = 'Peace' 
+  AND yr >= 2000
+
+SELECT yr, subject, winner
+  FROM nobel
+WHERE subject = 'Literature'
+  AND yr >= 1980 AND yr < 1990 
+
+SELECT * 
+FROM nobel
+  WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                  'Barack Obama')
+
