95cd4540546f21bf5315d4015521a3b939479af3
icebox827 <icebox827@gmail.com>
2021-01-07 20:43:22 -0500

    add 4 first answers

--- answer3.sql
+++ answer3.sql
@@ -17,3 +17,25 @@ SELECT *
 SELECT name
   FROM actor INNER JOIN movie ON actor.id = director
  WHERE gross < budget
+
+-- Usin null answer
+
+SELECT name
+FROM teacher
+WHERE dept IS NULL
+
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
+
+
+SELECT name, COALESCE(mobile, '07986 444 2266') as mobile
+FROM teacher;
