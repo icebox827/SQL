28527800f2ea744d25c853e18243c1c078155b6a
icebox827 <icebox827@gmail.com>
2021-01-07 19:02:09 -0500

    done witn join quiz

--- answer2.sql
+++ answer2.sql
@@ -27,3 +27,53 @@ SELECT team1, team2, player
   FROM game JOIN goal ON (id=matchid)
     WHERE player like 'Mario%'
 
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam ON (id=teamid)
+WHERE gtime<=10;
+
+SELECT mdate, teamname
+FROM game JOIN eteam ON (team1=eteam.id)
+WHERE coach = 'Fernando Santos' ;
+
+SELECT player
+FROM game JOIN goal ON (id=matchid)
+WHERE stadium =  'National Stadium, Warsaw';
+
+SELECT DISTINCT(player)
+FROM game JOIN goal ON matchid = id
+WHERE (team1='GER' OR team2='GER') AND teamid != 'GER';
+
+SELECT teamname, COUNT(player)
+FROM eteam JOIN goal ON id=teamid
+GROUP BY teamname
+ORDER BY teamname;
+
+SELECT stadium, COUNT(player)
+FROM game JOIN goal ON id=matchid
+GROUP BY stadium
+ORDER BY stadium;
+
+SELECT stadium, COUNT(player)
+FROM game JOIN goal ON id=matchid
+GROUP BY stadium
+ORDER BY stadium;
+
+-- Join Quiz
+
+SELECT DISTINCT player, teamid, gtime
+  FROM game JOIN goal ON matchid = id
+ WHERE stadium = 'Stadion Miejski (Wroclaw)'
+   AND (( teamid = team2 AND team1 != 'ITA') OR ( teamid = team1 AND team2 != 'ITA'))
+
+SELECT DISTINCT player, teamid 
+FROM game JOIN goal ON matchid = id 
+WHERE stadium = 'National Stadium, Warsaw' 
+AND (team1 = 'POL' OR team2 = 'POL')
+AND teamid != 'POL'
+
+SELECT player, teamid, COUNT(*)
+  FROM game JOIN goal ON matchid = id
+ WHERE (team1 = "GRE" OR team2 = "GRE")
+   AND teamid != 'GRE'
+ GROUP BY player, teamid
+
