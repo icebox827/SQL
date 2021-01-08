f7f85cbf030900ac81c30e3503306d157311b53e
icebox827 <icebox827@gmail.com>
2021-01-07 20:53:07 -0500

    done with null answer

--- answer3.sql
+++ answer3.sql
@@ -39,3 +39,26 @@ SELECT teacher.name, dept.name
 
 SELECT name, COALESCE(mobile, '07986 444 2266') as mobile
 FROM teacher;
+
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept
+    ON (teacher.dept=dept.id);
+
+SELECT COUNT(name) as Teachers, COUNT(mobile) as Numbers
+FROM teacher;
+
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept
+    ON (teacher.dept=dept.id)
+GROUP BY dept.name;
+
+SELECT name,
+    CASE WHEN dept = 1 OR dept = 2 THEN 'Sci' 
+    ELSE 'Art' END
+FROM teacher;
+
+SELECT name,
+    CASE WHEN dept = 1 OR dept = 2 THEN 'Sci' 
+    WHEN dept = 3 THEN 'Art' 
+    ELSE 'None' END
+FROM teacher;
