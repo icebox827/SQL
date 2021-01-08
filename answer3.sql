63d95d07ae7c8b4b86b2fc99a3c00e44f386f150
icebox827 <icebox827@gmail.com>
2021-01-07 19:52:11 -0500

    start more join quiz

---
+++ answer3.sql
@@ -0,0 +1,15 @@
+-- More join quiz
+
+SELECT name
+  FROM movie JOIN casting ON movie.id = movieid
+  JOIN actor ON actor.id = actorid
+WHERE ord = 1 AND director = 351
+
+SELECT name, COUNT(movieid)
+  FROM casting JOIN actor ON actorid=actor.id
+ WHERE name LIKE 'John %'
+ GROUP BY name ORDER BY 2 DESC
+
+SELECT *
+  FROM actor JOIN casting ON actor.id = actorid
+   JOIN movie ON movie.id = movieid
