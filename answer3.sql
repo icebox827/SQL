6f380eeb1190ff1eb94159aac947d5d152f5f618
icebox827 <icebox827@gmail.com>
2021-01-07 21:22:53 -0500

    Add first 5 answers

--- answer3.sql
+++ answer3.sql
@@ -70,3 +70,29 @@ SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = 
 SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
 
 SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id)
+
+-- Self answer
+
+SELECT COUNT(id)
+FROM stops;
+
+SELECT id
+FROM stops
+WHERE name = 'Craiglockhart';
+
+SELECT id, name
+FROM stops JOIN route ON (id = route.stop)
+WHERE num = 4 AND company = 'LRT'
+ORDER BY pos;
+
+SELECT company, num, COUNT(*)
+FROM route
+WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) = 2;
+
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop = 149;
+
